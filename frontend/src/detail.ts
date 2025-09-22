94b836e75bd6a2ce5888e7bc724b25953b88cb88
VRT <136970635+Varintornlu@users.noreply.github.com>
2025-09-22 18:08:27 +0700

    up

--- frontend/src/detail.ts
+++ frontend/src/detail.ts
@@ -3,5 +3,12 @@ import { createNavbar } from "./navbar.js";
 createNavbar();
 
 const title = document.createElement("h1");
-title.textContent = "Detail Page";
+title.textContent = "ชื่อเรื่อง(ปี)";
+title.style.margin = "20px";
 document.body.appendChild(title);
+
+const description = document.createElement("p");
+description.textContent =
+  "คำอธิบายย่อ ๆ เกี่ยวกับชื่อเรื่องนี้ คำอธิบายย่อ ๆ เกี่ยวกับชื่อเรื่องนี้ คำอธิบายย่อ ๆ เกี่ยวกับชื่อเรื่องนี้ คำอธิบายย่อ ๆ เกี่ยวกับชื่อเรื่องนี้ คำอธิบายย่อ ๆ เกี่ยวกับชื่อเรื่องนี้";
+description.style.margin = "20px";
+document.body.appendChild(description);
