1661e59e7dc010a42ec28a16a1c9f3cf2a8e5561
17punchisama <144563556+17punchisama@users.noreply.github.com>
2025-09-22 18:28:59 +0700

    add new font

--- frontend/src/home.ts
+++ frontend/src/home.ts
@@ -1,23 +1,15 @@
 import { createNavbar } from "./navbar.js";
-
 createNavbar();
-
-// const title = document.createElement("h1");
-// title.textContent = "Welcome to Home Page";
-// document.body.appendChild(title);
 
 const onairing = document.createElement("p");
 onairing.textContent = "กำลังออนแอร์";
-onairing.style.fontSize = "48"
-onairing.style.fontWeight = "bold";
+onairing.className = "banner-text";
+document.body.appendChild(onairing);
 
 const banner = document.createElement("div");
-banner.style.width = "100%";
-banner.style.height = "20vh";
-banner.style.width = "80vw";
-banner.style.display = "flex";
-banner.style.alignItems = "center";
-banner.style.backgroundColor = "#000";
-
-document.body.prepend(onairing);
-document.body.prepend(banner);
+// banner.textContent = "คลิกเพื่อดูรายละเอียด";
+banner.className = "banner-ad";
+banner.addEventListener("click", () => {
+    window.location.href = "detail.html";
+});
+document.body.appendChild(banner);
