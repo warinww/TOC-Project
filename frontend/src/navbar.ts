71ba51fb7de0fb372ce6a1c897977727a8b83d59
sgissweet <nitkamon.67908@gmail.com>
2025-09-22 19:47:08 +0700

    icon

--- frontend/src/navbar.ts
+++ frontend/src/navbar.ts
@@ -1,22 +1,33 @@
 export function createNavbar() {
     const nav = document.createElement("nav");
-    nav.style.padding = "10px";
-    nav.style.backgroundColor = "#333";
-    nav.style.color = "white";
+    nav.className = "navbox";
 
-    const homeLink = document.createElement("a");
-    homeLink.href = "index.html";
-    homeLink.textContent = "Home";
-    homeLink.style.marginRight = "15px";
-    homeLink.style.color = "white";
+    // const homeLink = document.createElement("a");
+    // homeLink.href = "index.html";
+    // homeLink.textContent = "Home";
+    // homeLink.style.marginRight = "15px";
+    // homeLink.style.color = "white";
 
-    const detailLink = document.createElement("a");
-    detailLink.href = "detail.html";
-    detailLink.textContent = "Detail";
-    detailLink.style.color = "white";
+    // const detailLink = document.createElement("a");
+    // detailLink.href = "detail.html";
+    // detailLink.textContent = "Detail";
+    // detailLink.style.color = "white";
 
-    nav.appendChild(homeLink);
-    nav.appendChild(detailLink);
+    const searchBar = document.createElement("div");
+    
+    const darkMode = document.createElement("img");
+    darkMode.src = "./public/dark_mode.svg";
+    darkMode.className = "colorMode";
+
+    const lightMode = document.createElement("img");
+    lightMode.src = "./public/light_mode.svg";
+    lightMode.className = "colorMode";
+
+    // nav.appendChild(homeLink);
+    // nav.appendChild(detailLink);
+    nav.appendChild(searchBar);
+    nav.appendChild(darkMode);
+    nav.appendChild(lightMode);
 
     document.body.prepend(nav);
 }
