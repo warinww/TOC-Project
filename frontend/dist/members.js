cbddf2b5ac0899c505c73549651f0dba84c26ed5
sgissweet <nitkamon.67908@gmail.com>
2025-09-29 19:29:38 +0700

    test

---
+++ frontend/dist/members.js
@@ -0,0 +1,10 @@
+"use strict";
+// import { createNavbar } from "./navbar.js";
+// import { createFooter } from "./footer.js";
+// createNavbar()
+// createFooter()
+const gobackdiv = document.createElement("div");
+const backicon = document.createElement("img");
+backicon.src = "";
+document.body.appendChild(gobackdiv);
+//# sourceMappingURL=members.js.map
---
+++ frontend/src/members.ts
@@ -0,0 +1,12 @@
+// import { createNavbar } from "./navbar.js";
+// import { createFooter } from "./footer.js";
+
+// createNavbar()
+// createFooter()
+
+const gobackdiv = document.createElement("div");
+const backicon = document.createElement("img");
+backicon.src = ""
+
+
+document.body.appendChild(gobackdiv);
