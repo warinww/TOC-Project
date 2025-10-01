16efc7e519b6ea2aab36f881b69617a592e14812
VRT <136970635+Varintornlu@users.noreply.github.com>
2025-10-01 21:27:43 +0700

    t

--- backend/poryor_test.py
+++ backend/poryor_test.py
@@ -6,8 +6,8 @@ from fastapi.staticfiles import StaticFiles
 from threading import Lock
 import time
 from home_crawl import get_casting_by_URL
-from fastapi.responses import FileResponse
-
+from fastapi.responses import FileResponse, Response
+import requests
 
 from home_crawl import (  # หรือ crawl.py ของคุณ
     POSTER_DIR,
